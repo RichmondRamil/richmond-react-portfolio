b2a01270efe23a7c67d02168e5e5b53ae46a17f6
RichmondRamil <richmondlimar214@gmail.com>
2021-12-20 11:45:16 +0800

    new add

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -17,9 +17,9 @@ const Projects = () => {
             <div className="mon__projects-cards__body text-dark">
               <h4 className="card-title">Aviary</h4>
               <p className="card-text text-dark">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                perspiciatis voluptatem eum, quas voluptatibus cum fugit maiores
-                vitae eveniet ipsa.
+                This is the first website that I made on my friends business
+                which is they are selling own breed cockatiel birds. This
+                busines is located in pangasinan.
               </p>
               <a href="#" className="btn btn-success">
                 Download Files
@@ -33,11 +33,11 @@ const Projects = () => {
               <img src={campbakes} className="card-img-top" alt="" />
             </div>
             <div className="mon__projects-cards__body text-dark">
-              <h4 className="card-title">Card Title</h4>
+              <h4 className="card-title">Camp Bakes Bakery</h4>
               <p className="card-text text-dark">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                perspiciatis voluptatem eum, quas voluptatibus cum fugit maiores
-                vitae eveniet ipsa.
+                This is the second website that I created for my cousins
+                business to be able him to show case his own baked styles to his
+                customers.
               </p>
               <a href="#" className="btn btn-success">
                 Download Files
@@ -51,11 +51,11 @@ const Projects = () => {
               <img src={passengercounter} className="card-img-top" alt="" />
             </div>
             <div className="mon__projects-cards__body text-dark">
-              <h4 className="card-title">Card Title</h4>
+              <h4 className="card-title">Passenger Counter</h4>
               <p className="card-text text-dark">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                perspiciatis voluptatem eum, quas voluptatibus cum fugit maiores
-                vitae eveniet ipsa.
+                This is the project that I created after reading, watching some
+                tutorials of JavaScript , also practicing and applying my
+                knowledge on ReactJS
               </p>
               <a href="#" className="btn btn-success">
                 Download Files
@@ -69,11 +69,10 @@ const Projects = () => {
               <img src={calculator} className="card-img-top" alt="" />
             </div>
             <div className="mon__projects-cards__body text-dark">
-              <h4 className="card-title">Card Title</h4>
+              <h4 className="card-title">Calculator</h4>
               <p className="card-text text-dark">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                perspiciatis voluptatem eum, quas voluptatibus cum fugit maiores
-                vitae eveniet ipsa.
+                This is the first practice of my JavaScript Learning when I was
+                3rd year College
               </p>
               <a href="#" className="btn btn-success">
                 Download Files
