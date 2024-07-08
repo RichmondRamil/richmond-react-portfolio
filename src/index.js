aacb08ad02f02551a5db00b12b0a8a3187f3bccd
Richmond <toro.richmond.ramil@gmail.com>
2024-07-08 21:14:59 +0800

    chore: Add @actions/core dependency and update npm packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import core from 'actions/core'
 
-import App from "./App";
-import "./index.css";
-ReactDOM.render(<App />, document.getElementById("root"));
+const person = core.getInput('person');
+
+console.log(`Welcome ${person}!`);
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+// import "./index.css";
+// ReactDOM.render(<App />, document.getElementById("root"));
